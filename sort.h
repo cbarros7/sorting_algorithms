53719b498e0cb1e7baf9e162931266b0a697ad1f
cbarros7 <cbarros7>
2020-06-16 20:32:09 -0700

    Add new functions

--- sort.h
+++ sort.h
@@ -27,4 +27,8 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
+/*Other functions*/
+int lomuto_partition(int *array, int start, int end, size_t size);
+void lomuto_sort(int *array, int start, int end, size_t size);
+
 #endif
